8e2351ec2a50d05bdb03ec3614d3c1c7795e4e16
mutagatifu <maicseba@gmail.com>
2023-10-21 18:31:17 +0000

    db file config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_new' );
+define( 'DB_NAME', 'wordpress_news' );
 
 /** Database username */
 define( 'DB_USER', 'mavin' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'mavin123' );
+define( 'DB_PASSWORD', 'mavin12345' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
